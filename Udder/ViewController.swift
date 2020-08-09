11aac23230f6850e05435d29d70ac6bc7d4476a3
Ashish Mittal <ashish.yugasa@gmail.com>
2020-08-09 17:58:46 +0530

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ashish Mittal on 09/08/20.
+//  Copyright © 2020 Yugasa Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
